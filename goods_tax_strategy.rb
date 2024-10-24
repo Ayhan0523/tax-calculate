51ebd4aaff25a175ed2d6124c0a6deefc14602ca
Ayhan0523 <ayhanozu0805@gmail.com>
2024-10-24 06:28:14 -0700

    create goods_tax_strategy

---
+++ goods_tax_strategy.rb
@@ -0,0 +1,14 @@
+require_relative 'tax_strategy'
+
+class GoodsTaxStrategy < TaxStrategy
+  def calculate(transaction, vat_service)
+    if transaction.buyer_in_spain?
+      return vat_service.fetch_rate("ES")
+    elsif transaction.buyer_in_eu?
+      return transaction.buyer_is_company? ? 0 : vat_service.fetch_rate(transaction.buyer_country)
+    else
+      return 0 #Export, no tax applied
+    end
+  end
+end
+  
