480e4147e02e5abf54189c3004ae0dbb6785ad3b
Ayhan0523 <ayhanozu0805@gmail.com>
2024-10-24 08:14:58 -0700

    fixed the grammer error

--- digital_service_tax_strategy.rb
+++ digital_service_tax_strategy.rb
@@ -4,7 +4,7 @@ class DigitalServiceTaxStrategy < TaxStrategy
   def calculate(transaction, vat_service)
     if transaction.buyer_in_spain?
       return vat_service.fetch_rate("ES")
-    elseif transaction.buyer_in_eu?
+    elsif transaction.buyer_in_eu?
       return transaction.buyer_is_company? ? 0 : vat_service.fetch_rate(transaction.buyer_country)
     else
       return 0 #No tax for non-EU countries
