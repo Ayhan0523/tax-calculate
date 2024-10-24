98fab37da213f2e307d4ba6f21f97690dcd826f4
Ayhan0523 <ayhanozu0805@gmail.com>
2024-10-24 06:30:54 -0700

    crete digital service tax stragety

---
+++ digital_service_tax_strategy.rb
@@ -0,0 +1,14 @@
+require_relative 'tax_strategy'
+
+class DigitalServiceTaxStrategy < TaxStrategy
+  def calculate(transaction, vat_service)
+    if transaction.buyer_in_spain?
+      return vat_service.fetch_rate("ES")
+    elseif transaction.buyer_in_eu?
+      return transaction.buyer_is_company? ? 0 : vat_service.fetch_rate(transaction.buyer_country)
+    else
+      return 0 #No tax for non-EU countries
+    end
+  end
+end
+  
